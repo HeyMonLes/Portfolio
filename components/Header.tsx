71d7996259938a8ed2ae50ab8b064556fa46ff21
lesmon <lesmonnn@gmail.com>
2022-12-13 18:03:40 +0800

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,33 +5,36 @@ type Props = {};
 
 function Header({}: Props) {
   return (
-    <div className="flex flex-row items-center">
-      <SocialIcon
-        url="https://www.youtube.com/@monles_yen"
-        fgColor="gray"
-        bgColor="transparent"
-      />
-      <SocialIcon
-        url="https://www.youtube.com/@monles_yen"
-        fgColor="gray"
-        bgColor="transparent"
-      />
-      <SocialIcon
-        url="https://www.youtube.com/@monles_yen"
-        fgColor="gray"
-        bgColor="transparent"
-      />
-
-      <SocialIcon
-        className="cursor-pointer"
-        network="email"
-        fgColor="gray"
-        bgColor="transparent"
-      />
-      <p className="uppercase hidden md:inline-flex text-sm text-gray">
-        Get In Touch
-      </p>
-    </div>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <div className="flex flex-row items-center">
+        <SocialIcon
+          url="https://www.youtube.com/@monles_yen"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com/@monles_yen"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.youtube.com/@monles_yen"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray">
+          Get In Touch
+        </p>
+      </div>
+    </header>
   );
 }
 
